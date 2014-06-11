3d58c6d4b25faefaf5610a7d87b47f578c0e2ecc
Matt Stine <mstine@gopivotal.com>
2014-06-10 23:38:23 -0700

    initial implementation of catalog endpoint

---
+++ src/main/java/com/mattstine/cf/srb/model/Plan.java
@@ -0,0 +1,42 @@
+package com.mattstine.cf.srb.model;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "plans")
+public class Plan {
+
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/mattstine/cf/srb/repository/ServiceRepository.java
@@ -0,0 +1,7 @@
+package com.mattstine.cf.srb.repository;
+
+import com.mattstine.cf.srb.model.Service;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ServiceRepository extends CrudRepository<Service, String> {
+}
